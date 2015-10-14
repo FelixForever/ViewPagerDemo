f94709b7d8274d1c5f9eec2d14bc5c223de9cffa
felix <736232402@qq.com>
2015-10-14 17:08:49 +0800

    modify1

--- ViewpagerDemo1/src/com/example/viewpagerdemo1/MainActivity.java
+++ ViewpagerDemo1/src/com/example/viewpagerdemo1/MainActivity.java
@@ -1,18 +1,45 @@
 package com.example.viewpagerdemo1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private ViewPager mViewPager;
+	private List<View> mViews;
 
-   
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mViewPager = (ViewPager) findViewById(R.id.vp_main);
+		//mViewPager=new ArrayList<E>();
+	}
+
+	class AdpViewPager extends PagerAdapter {
+		List<View> viewlist;
+
+		AdpViewPager(List<View> list) {
+			viewlist = list;
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return viewlist.size();
+		}
+
+		@Override
+		public boolean isViewFromObject(View arg0, Object arg1) {
+			// TODO Auto-generated method stub
+			return arg0 == arg1;
+		}
+
+	}
 }
